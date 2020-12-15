a15fd33c05543722d0467e51d928ae4e47ba9467
unknown <luki111.1998@gmail.com>
2020-12-15 12:06:36 +0100

    1.3 Zmiana wciec w liniach 5,13,14,15

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,7 +1,5 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.defaultCountingOutRhymer;
-import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
@@ -20,7 +18,7 @@ class RhymersDemo {
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(20));
-		
+
 		for (int i = 0; i < rhymers.length; i++) {
 			while (!rhymers[i].callCheck())
 				System.out.print(rhymers[i].countOut() + "  ");
