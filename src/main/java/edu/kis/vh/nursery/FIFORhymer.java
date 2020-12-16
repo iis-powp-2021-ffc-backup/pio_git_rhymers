3d20eefbe7587660c8071cb58f89e0b2f062f2a7
Bartłomiej Świstak <bartekswistak20@gmail.com>
2020-12-16 15:21:12 +0100

    1.3 files formatted

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,21 +1,18 @@
 package edu.kis.vh.nursery;
-
+//kombinacja alt + <- w Intellij zmienia kartę z wcześniej otwartym plikiem
 public class FIFORhymer extends defaultCountingOutRhymer {
 
-	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-	
-	@Override
-	public int countOut() {
-		while (!callCheck())
-			
-		temp.countIn(super.countOut());
-		
-		int ret = temp.countOut();
-		
-		while (!temp.callCheck())
-			
-		countIn(temp.countOut());
-		
-		return ret;
-	}
+    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+
+    @Override
+    public int countOut() {
+        while (!callCheck())
+            temp.countIn(super.countOut());
+
+        int ret = temp.countOut();
+
+        while (!temp.callCheck())
+            countIn(temp.countOut());
+        return ret;
+    }
 }
