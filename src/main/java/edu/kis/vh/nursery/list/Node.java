3bcd1c605cbfd0d82f8109e60ae473854cd80a4f
Whitesan <piotrekrek12@wp.pl>
2020-12-16 15:22:33 +0100

    files formatted

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,12 +1,12 @@
 package edu.kis.vh.nursery.list;
-
+// kombinacja alt + arrow przenosi nas na sąsiadującą otwartą karte w naszym środowisku
 public class Node {
 
-	public int value;
-	public Node prev, next;
+    public int value;
+    public Node prev, next;
 
-	public Node(int i) {
-		value = i;
-	}
-	
+    public Node(int i) {
+        value = i;
+    }
+
 }
