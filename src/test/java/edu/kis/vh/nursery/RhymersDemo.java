b775f100f4bef86c497f5b31ce0a0285e2bf692a
Lukas <luki111.1998@gmail.com>
2021-01-14 21:16:06 +0100

    Utworzenie metody testRhymers

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,14 +7,18 @@ class RhymersDemo {
 
 	public static void main(String[] args) {
 		Rhymersfactory factory = new DefaultRhymersFactory();
-		
+
+		testRhymers(factory);
+	}
+
+	private static void testRhymers(Rhymersfactory factory) {
 		defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
 				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-		
+
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				rhymers[j].countIn(i);
-		
+
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(20));
@@ -24,10 +28,9 @@ class RhymersDemo {
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
 		}
-		
+
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
 	}
-	
+
 }
