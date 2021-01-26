49d34af0e9bd8fc3207d18ff4d4d64bbaa26212c
TinyRogue <oskarmikus@protonmail.com>
2021-01-26 10:48:35 +0100

    Task 2.0

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,40 +2,37 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
+    private final IntArrayStack intArrayStack;
 
-    private static int EMPTY_RHYMER_INDICATOR = -1;
-    private final static int IS_EMPTY = -1;
-    private final static int CAPACITY = 12;
-    private final int[] numbers = new int[CAPACITY];
+    public DefaultCountingOutRhymer() {
+        intArrayStack = new IntArrayStack();
+    }
+
+    public DefaultCountingOutRhymer(IntArrayStack intArrayStack) {
+        this.intArrayStack = intArrayStack;
+    }
 
     public int getTotal() {
-        return EMPTY_RHYMER_INDICATOR;
+        return intArrayStack.getTotal();
     }
 
     public void countIn(int in) {
-        if (!isFull())
-            numbers[++EMPTY_RHYMER_INDICATOR] = in;
+        intArrayStack.countIn(in);
     }
 
     public boolean callCheck() {
-        return EMPTY_RHYMER_INDICATOR == IS_EMPTY;
+        return intArrayStack.callCheck();
     }
 
     public boolean isFull() {
-        return EMPTY_RHYMER_INDICATOR == CAPACITY - 1;
+        return intArrayStack.isFull();
     }
 
-    //TODO: Add brackets to if for better readability and avoiding unintended bugs
-    protected int peekaboo() {
-        if (callCheck())
-            return IS_EMPTY;
-        return numbers[EMPTY_RHYMER_INDICATOR];
+    public int peekaboo() {
+        return intArrayStack.peekaboo();
     }
 
-    //TODO: Add brackets to if for better readability and avoiding unintended bugs
     public int countOut() {
-        if (callCheck())
-            return IS_EMPTY;
-        return numbers[EMPTY_RHYMER_INDICATOR--];
+        return intArrayStack.countOut();
     }
 }
---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,39 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+    private static int EMPTY_RHYMER_INDICATOR = -1;
+    private final static int IS_EMPTY = -1;
+    private final static int CAPACITY = 12;
+    private final int[] numbers = new int[CAPACITY];
+
+    public int getTotal() {
+        return EMPTY_RHYMER_INDICATOR;
+    }
+
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++EMPTY_RHYMER_INDICATOR] = in;
+    }
+
+    public boolean callCheck() {
+        return EMPTY_RHYMER_INDICATOR == IS_EMPTY;
+    }
+
+    public boolean isFull() {
+        return EMPTY_RHYMER_INDICATOR == CAPACITY - 1;
+    }
+
+    //TODO: Add brackets to if for better readability and avoiding unintended bugs
+    protected int peekaboo() {
+        if (callCheck())
+            return IS_EMPTY;
+        return numbers[EMPTY_RHYMER_INDICATOR];
+    }
+
+    //TODO: Add brackets to if for better readability and avoiding unintended bugs
+    public int countOut() {
+        if (callCheck())
+            return IS_EMPTY;
+        return numbers[EMPTY_RHYMER_INDICATOR--];
+    }
+}
